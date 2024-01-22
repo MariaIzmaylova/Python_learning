3f202c2fca7ff2977ef21e886b20fdd1f0825072
Your Name <kraynova-m-a@inbox.ru>
2024-01-22 22:15:10 +0600

    исправлены ошибки в коде

--- count.py
+++ count.py
@@ -1,31 +1,33 @@
+import time
 count = 0
-while (True):
-	print(count)
-	count+=1
-	time.sleep(.500)
+while (count<6):
+    print(count)
+    count+=1
+    time.sleep(.500)
 	
 count=0
 while(count<10):
-	print('iter=', count)
-	time.sleep(.500)
+    count+=5
+    print('iter=', count)
+    time.sleep(.500)
 
 count=0
 iter_count=1
 while(count<=10):
-	print(iter_count, '--iter=', count)
-	count+=1
-	iter_count+=1
-	time.sleep(.500)
+    print(iter_count, '--iter=', count)
+    count+=1
+    iter_count+=1
+    time.sleep(.500)
 	
 
 ll=[11,22,33,44]
 for i in range(0,10):
-	print(i)
-	time.sleep(.200)
+    print(i)
+    time.sleep(.200)
 	
-ll=[11,22,33,44,55 ['qq','ww','ee','rr','tt','yy'],66,77]
+ll=[11,22,33,44,55,['qq','ww','ee','rr','tt','yy'],66,77]
 count=0
 for i in ll:
-	print(count,ll[count])
-	count+=1
-	time.sleep(.200)
+    print(count,ll[count])
+    count+=1
+    time.sleep(.200)
