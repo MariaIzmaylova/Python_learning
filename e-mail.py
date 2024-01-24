dd09de4c892cd0946bdc9f0fc5a776505e31f743
Your Name <kraynova-m-a@inbox.ru>
2024-01-24 15:05:19 +0600

    генератор почтовых адресов

---
+++ e-mail.py
@@ -0,0 +1,6 @@
+#импортировать библиотеку имен в интерпритатор через терминал: pip3 install names
+for i in range(0,10):
+    nn1 = names.get_first_name()
+	nn2 = names.get_last_name()
+	result=nn1+nn2+@gmail.com
+	print(i, result)
